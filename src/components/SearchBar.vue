7e7c82c2da6d943b55a958e5a62ef24629ed90b9
ghostrib <sh4rkbot@gmail.com>
2020-02-02 18:27:45 +0100

    add search bar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" @input="handleChange" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    handleChange: function(event) {
+      alert(event.target.value);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
